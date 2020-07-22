f587001915b13eeda1997d3b9732012e29709cd4
Abdullah Mughrabi <atmughra@ncsu.edu>
2020-07-22 03:23:02 -0400

    [FIXEDPOINT|LIB] fixed scale for fixed to float

--- 00_graph_bench/include/utils/fixedPoint.h
+++ 00_graph_bench/include/utils/fixedPoint.h
@@ -15,7 +15,7 @@
 #define SCALEF_8  6 // 1/2^16
 #define SCALEF_16 14 // 1/2^16
 
-#define SCALEF 28 // 1/2^16
+#define SCALEF 16 // 1/2^16
 #define SCALED 32 // 1/2^32
 #define EPSILON 1  // smallest possible increment or decrement you can perform
 #define FRACTION_MASK_32 (0xFFFFFFFF >> (32-SCALEF))
@@ -37,7 +37,7 @@
 #define Fixed64ToDouble(num)	((double)(num) / (double)((uint64_t)(1)<<SCALED))
 
 #define Fixed32ToFloat(num)	((float)(num) / (float)((uint32_t)(1)<<SCALEF))
-#define Fixed64ToFloat(num)	((float)(num) / (float)((uint64_t)(1)<<SCALEF))
+#define Fixed64ToFloat(num)	((float)(num) / (float)((uint64_t)(1)<<SCALED))
 #define Fixed16ToFloat(num)	((float)(num) / (float)((uint16_t)(1)<<SCALEF_16))
 #define Fixed8ToFloat(num)	((float)(num) / (float)((uint8_t)(1) <<SCALEF_8))
 
