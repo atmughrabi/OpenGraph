0a77e2b0fbe68ff97bba6f60b03ffe159eca7029
Abdullah Mughrabi <atmughra@ncsu.edu>
2021-10-25 14:02:28 -0400

    [REORDER] update masked reordering

--- 00_graph_bench/src/preprocess/reorder.c
+++ 00_graph_bench/src/preprocess/reorder.c
@@ -1332,9 +1332,9 @@ struct EdgeList *maskGraphProcessGenerateMaskArray(struct EdgeList *edgeList, ui
 
     if(diff < (2 * (int)cache_size))
     {
-        cache_regions[0] = edgeList->num_vertices / 3; // VERTEX_VALUE_HOT_U32
-        cache_regions[1] = edgeList->num_vertices / 3; // VERTEX_CACHE_WARM_U32
-        cache_regions[2] = edgeList->num_vertices / 3; // VERTEX_VALUE_LUKEWARM_U32
+        cache_regions[0] = edgeList->num_vertices / num_masks; // VERTEX_VALUE_HOT_U32
+        cache_regions[1] = edgeList->num_vertices / num_masks; // VERTEX_CACHE_WARM_U32
+        cache_regions[2] = edgeList->num_vertices / num_masks; // VERTEX_VALUE_LUKEWARM_U32
     }
     else
     {
